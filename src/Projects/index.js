089e2599f650b524e215bd74a757536d5f4a669d
Your Name <leobarrientos02@gmail.com>
2022-02-23 00:38:28 -0500

    added more projectss

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -6,6 +6,12 @@ import Iphone from '../img/iPhone13Page.png';
 import Moda from '../img/FashionPage.png';
 import Photon from '../img/photon.jpg';
 import Elena from '../img/elena-joy.jpg';
+import ColorApp from '../img/ColorApp.jpg';
+import Culture from '../img/culture.jpg';
+import Beats from '../img/beats_maker.jpg';
+import Todo from '../img/todo.jpg';
+import Calc from '../img/calculator.jpg';
+
 export const projectData = [
     {
         name: 'Travelly',
@@ -66,10 +72,50 @@ export const projectData = [
     {
         name: 'Elena Gallery',
         image: Elena,
-        // link: "https://focused-archimedes-c341c5.netlify.app/",
+        link: "https://color-chooser-app.netlify.app/",
         github: 'https://github.com/leobarrientos02/Photography_Site',
         technology: ['HTML', "CSS", "JavaScript"],
         description: "Beautiful UX/UI design for professional photographers wanting to display their work online.",
+    },
+    {
+        name: 'ColorApp',
+        image: ColorApp,
+        link: "https://focused-archimedes-c341c5.netlify.app/",
+        github: 'https://github.com/leobarrientos02/ColorApp',
+        technology: ['HTML', "CSS", "JavaScript"],
+        description: "An app that generates color palletes for the user and also allows to visuallize the color patterns wanted for a project.",
+    },
+    {
+        name: 'ToDo App',
+        image: Todo,
+        link: "https://affectionate-hamilton-d96896.netlify.app/",
+        github: 'https://github.com/leobarrientos02/ToDoApp',
+        technology: ['HTML', "CSS", "JavaScript"],
+        description: "Simple ToDo App that i developed to master Javascript and dom manipulation.",
+    },
+    {
+        name: 'Culture',
+        image: Culture,
+        link: "https://culture-love.netlify.app/",
+        github: 'https://github.com/leobarrientos02/WebsiteDesign',
+        technology: ['HTML', "CSS", "JavaScript", "Babel"],
+        description: "Beautiful UX/UI design that uses amazing page transitions to have a good user experience.",
+    },
+    {
+        name: 'Beats App',
+        image: Beats,
+        link: "https://makeyourownbeat.netlify.app/",
+        github: 'https://github.com/leobarrientos02/Beat_Maker',
+        technology: ['HTML', "CSS", "JavaScript"],
+        description: "An adavanced app that allows users to make their own beats using available sounds.",
+    },
+    {
+        name: 'Calculator',
+        image: Calc,
+        link: "https://epic-mahavira-ca42eb.netlify.app/",
+        github: 'https://github.com/leobarrientos02/Simple_Calculator',
+        technology: ['HTML', "CSS", "JavaScript"],
+        description: "A simple calculator that allows users to do math calculations. Includes Math Laws.",
     },
 
 ];
