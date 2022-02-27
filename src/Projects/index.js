73c78be44891b4fa3dff63ceef1709a14a8a86ad
Your Name <leobarrientos02@gmail.com>
2022-02-27 01:36:53 -0500

    new project added

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,175 +1,201 @@
-import Travelly from '../img/travelly.jpg';
-import Inferno from '../img/inferno.png';
-import Capture from '../img/capture.png';
-import Waves from '../img/musicApp.jpg';
-import Iphone from '../img/iPhone13Page.png';
-import Moda from '../img/FashionPage.png';
-import Photon from '../img/photon.jpg';
-import Elena from '../img/elena-joy.jpg';
-import ColorApp from '../img/ColorApp.jpg';
-import Culture from '../img/culture.jpg';
-import Beats from '../img/beats_maker.jpg';
-import Todo from '../img/todo.jpg';
-import Calc from '../img/calculator.jpg';
-import TravelAdvisor from '../img/TravelAdvisor.png';
-import Cryptoverse from '../img/Cryptoverse.png';
-import GoogLeo from '../img/GoogLeo.png';
-import RealEstate from '../img/RealEstate.png';
-
+import Travelly from "../img/travelly.jpg";
+import Inferno from "../img/inferno.png";
+import Capture from "../img/capture.png";
+import Waves from "../img/musicApp.jpg";
+import Iphone from "../img/iPhone13Page.png";
+import Moda from "../img/FashionPage.png";
+import Photon from "../img/photon.jpg";
+import Elena from "../img/elena-joy.jpg";
+import ColorApp from "../img/ColorApp.jpg";
+import Culture from "../img/culture.jpg";
+import Beats from "../img/beats_maker.jpg";
+import Todo from "../img/todo.jpg";
+import Calc from "../img/calculator.jpg";
+import TravelAdvisor from "../img/TravelAdvisor.png";
+import Cryptoverse from "../img/Cryptoverse.png";
+import GoogLeo from "../img/GoogLeo.png";
+import RealEstate from "../img/RealEstate.png";
+import EZWork from "../img/ezwork.png";
 
 export const projectData = [
-    {
-        name: 'Travel Adivisor',
-        image: TravelAdvisor,
-        link: "https://traveladvisorlb.netlify.app/",
-        github: 'https://github.com/leobarrientos02/travel_advisor',
-        technology: ["React", "React-Router", "API", "Axios"],
-        description: "Travel Advisor uses Google Maps Api which helps people locate hotels, attractions and restaurants and weather in their area or where ever they want. ",
-        tech: "React",
-    },
-    {
-        name: 'Real Estate App',
-        image: RealEstate,
-        link: "https://react-real-estate-lyart.vercel.app/",
-        github: 'https://github.com/leobarrientos02/react-real-estate',
-        technology: ["React", "Axios", "Next.js", "Framer-Motion", "ChakraUI"],
-        description: "Real Estate App that uses an API to get data on real-estate found in Dubai, app provides many ways to filter through the API.",
-        tech: "React"
-    },
-    {
-        name: 'Inferno Gaming',
-        image: Inferno,
-        link: "https://inferno-gaming.netlify.app/",
-        github: 'https://github.com/leobarrientos02/React-Gaming',
-        technology: ['React', "React Router", "Axios","Redux"],
-        description: "Website created using React.js and many other technologies that fetches data from an API and displays on video games.",
-        tech: "React",
-    },
-    {
-        name: 'Cryptoverse',
-        image: Cryptoverse,
-        link: "https://cryptocurrencyworldapp.netlify.app/",
-        github: 'https://github.com/leobarrientos02/cryptoapp',
-        technology: ['React', "React Router", "Axios","Redux", "API"],
-        description: "Crypto App was developed using multiple API's from RapidAPI.com, the app displays information on many crypto coins and news related to crypto.",
-        tech: "React",
-    },
-    {
-        name: 'GoogLeo',
-        image: GoogLeo,
-        link: "https://googleo.netlify.app/search",
-        github: 'https://github.com/leobarrientos02/googleo',
-        technology: ["React", "API", "Tailwind.css", "Axios"],
-        description: "GoogLeo is a google Replica using their own API, the app allows users to search for images, news, videos, and more.",
-        tech: "React",
-    },
-    {
-        name: 'Travelly',
-        image: Travelly,
-        link: "https://adoring-bell-b485f5.netlify.app/",
-        github: 'https://github.com/leobarrientos02/Travelly',
-        technology: ["HTML", "CSS", "JavaScript"],
-        description: "Website design for a travel agency, to help attract customers to book vacation plans with the company.",
-        tech: "UX/UI",
-    },
-    {
-        name: 'Capture',
-        image: Capture,
-        link: "https://wizardly-kepler-b13330.netlify.app/",
-        github: 'https://github.com/leobarrientos02/capture',
-        technology: ['React', "React Router", "SASS", "JavaScript"],
-        description: "UX/UI design for a movie review site, using React and Framer Motion.",
-        tech: "React",
-    },
-    {
-        name: 'Waves X',
-        image: Waves,
-        link: "https://music-x-waves.netlify.app/",
-        github: 'https://github.com/leobarrientos02/react-music',
-        technology: ['React', "SASS", "JavaScript"],
-        description: "Music App using React to allow users to listen to relaxing free music.",
-        tech: "React",
-    },
-    {
-        name: 'iPhone13 Replica',
-        image: Iphone,
-        link: "https://iphone13-replica.netlify.app/",
-        github: 'https://github.com/leobarrientos02/UltimateAnimationPage',
-        technology: ['HTML', "SASS", "JavaScript", "GSAP"],
-        description: "Making a website that uses many transitions such as page transitions, scroll triggers, & more.",
-        tech: "UX/UI",
-    },
-    {
-        name: 'Moda',
-        image: Moda,
-        link: "https://cool-fashion-webdesign.netlify.app/",
-        github: 'https://github.com/leobarrientos02/FashionSiteDesign',
-        technology: ['HTML', "SASS", "JavaScript", "GSAP", "Barba"],
-        description: "Fashion Site UX/UI that uses barba and gsap for awesome page transitions.",
-        tech: "UX/UI",
-    },
-    {
-        name: 'Photon',
-        image: Photon,
-        link: "https://photographer-work.netlify.app/",
-        github: 'https://github.com/leobarrientos02/Photon',
-        technology: ['HTML', "CSS", "JavaScript"],
-        description: "Photon allows users to search the Pexel API for any images they want.",
-        tech: "Javascript",
-    },
-    {
-        name: 'Elena Gallery',
-        image: Elena,
-        link: "https://focused-archimedes-c341c5.netlify.app/",
-        github: 'https://github.com/leobarrientos02/Photography_Site',
-        technology: ['HTML', "CSS", "JavaScript"],
-        description: "Beautiful UX/UI design for professional photographers wanting to display their work online.",
-        tech: "UX/UI",
-    },
-    {
-        name: 'ColorApp',
-        image: ColorApp,
-        link: "https://color-chooser-app.netlify.app/",
-        github: 'https://github.com/leobarrientos02/ColorApp',
-        technology: ['HTML', "CSS", "JavaScript"],
-        description: "An app that generates color palletes for the user and also allows to visuallize the color patterns wanted for a project.",
-        tech: "Javascript",
-    },
-    {
-        name: 'ToDo App',
-        image: Todo,
-        link: "https://affectionate-hamilton-d96896.netlify.app/",
-        github: 'https://github.com/leobarrientos02/ToDoApp',
-        technology: ['HTML', "CSS", "JavaScript"],
-        description: "Simple ToDo App that i developed to master Javascript and dom manipulation.",
-        tech: "Javascript",
-    },
-    {
-        name: 'Culture',
-        image: Culture,
-        link: "https://culture-love.netlify.app/",
-        github: 'https://github.com/leobarrientos02/WebsiteDesign',
-        technology: ['HTML', "CSS", "JavaScript", "Babel"],
-        description: "Beautiful UX/UI design that uses amazing page transitions to have a good user experience.",
-        tech: "UX/UI",
-    },
-    {
-        name: 'Beats App',
-        image: Beats,
-        link: "https://makeyourownbeat.netlify.app/",
-        github: 'https://github.com/leobarrientos02/Beat_Maker',
-        technology: ['HTML', "CSS", "JavaScript"],
-        description: "An adavanced app that allows users to make their own beats using available sounds.",
-        tech: "Javascript",
-    },
-    {
-        name: 'Calculator',
-        image: Calc,
-        link: "https://epic-mahavira-ca42eb.netlify.app/",
-        github: 'https://github.com/leobarrientos02/Simple_Calculator',
-        technology: ['HTML', "CSS", "JavaScript"],
-        description: "A simple calculator that allows users to do math calculations. Includes Math Laws.",
-        tech: "Javascript",
-    },
-
+  {
+    name: "Travel Adivisor",
+    image: TravelAdvisor,
+    link: "https://traveladvisorlb.netlify.app/",
+    github: "https://github.com/leobarrientos02/travel_advisor",
+    technology: ["React", "React-Router", "API", "Axios"],
+    description:
+      "Travel Advisor uses Google Maps Api which helps people locate hotels, attractions and restaurants and weather in their area or where ever they want. ",
+    tech: "React",
+  },
+  {
+    name: "Real Estate App",
+    image: RealEstate,
+    link: "https://react-real-estate-lyart.vercel.app/",
+    github: "https://github.com/leobarrientos02/react-real-estate",
+    technology: ["React", "Axios", "Next.js", "Framer-Motion", "ChakraUI"],
+    description:
+      "Real Estate App that uses an API to get data on real-estate found in Dubai, app provides many ways to filter through the API.",
+    tech: "React",
+  },
+  {
+    name: "Inferno Gaming",
+    image: Inferno,
+    link: "https://inferno-gaming.netlify.app/",
+    github: "https://github.com/leobarrientos02/React-Gaming",
+    technology: ["React", "React Router", "Axios", "Redux"],
+    description:
+      "Website created using React.js and many other technologies that fetches data from an API and displays on video games.",
+    tech: "React",
+  },
+  {
+    name: "Cryptoverse",
+    image: Cryptoverse,
+    link: "https://cryptocurrencyworldapp.netlify.app/",
+    github: "https://github.com/leobarrientos02/cryptoapp",
+    technology: ["React", "React Router", "Axios", "Redux", "API"],
+    description:
+      "Crypto App was developed using multiple API's from RapidAPI.com, the app displays information on many crypto coins and news related to crypto.",
+    tech: "React",
+  },
+  {
+    name: "GoogLeo",
+    image: GoogLeo,
+    link: "https://googleo.netlify.app/search",
+    github: "https://github.com/leobarrientos02/googleo",
+    technology: ["React", "API", "Tailwind.css", "Axios"],
+    description:
+      "GoogLeo is a google Replica using their own API, the app allows users to search for images, news, videos, and more.",
+    tech: "React",
+  },
+  {
+    name: "Travelly",
+    image: Travelly,
+    link: "https://adoring-bell-b485f5.netlify.app/",
+    github: "https://github.com/leobarrientos02/Travelly",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "Website design for a travel agency, to help attract customers to book vacation plans with the company.",
+    tech: "UX/UI",
+  },
+  {
+    name: "EZWork",
+    image: EZWork,
+    link: "https://ez-work.herokuapp.com/index",
+    github: "https://github.com/EzworkSeniorProject/EzWork",
+    technology: ["HTML", "SASS", "JavaScript", "PHP"],
+    description:
+      "Senior Project developed for a college course, partner handled the back-end & i did the front-end.",
+    tech: "System",
+  },
+  {
+    name: "Capture",
+    image: Capture,
+    link: "https://wizardly-kepler-b13330.netlify.app/",
+    github: "https://github.com/leobarrientos02/capture",
+    technology: ["React", "React Router", "SASS", "JavaScript"],
+    description:
+      "UX/UI design for a movie review site, using React and Framer Motion.",
+    tech: "React",
+  },
+  {
+    name: "Waves X",
+    image: Waves,
+    link: "https://music-x-waves.netlify.app/",
+    github: "https://github.com/leobarrientos02/react-music",
+    technology: ["React", "SASS", "JavaScript"],
+    description:
+      "Music App using React to allow users to listen to relaxing free music.",
+    tech: "React",
+  },
+  {
+    name: "iPhone13 Replica",
+    image: Iphone,
+    link: "https://iphone13-replica.netlify.app/",
+    github: "https://github.com/leobarrientos02/UltimateAnimationPage",
+    technology: ["HTML", "SASS", "JavaScript", "GSAP"],
+    description:
+      "Making a website that uses many transitions such as page transitions, scroll triggers, & more.",
+    tech: "UX/UI",
+  },
+  {
+    name: "Moda",
+    image: Moda,
+    link: "https://cool-fashion-webdesign.netlify.app/",
+    github: "https://github.com/leobarrientos02/FashionSiteDesign",
+    technology: ["HTML", "SASS", "JavaScript", "GSAP", "Barba"],
+    description:
+      "Fashion Site UX/UI that uses barba and gsap for awesome page transitions.",
+    tech: "UX/UI",
+  },
+  {
+    name: "Photon",
+    image: Photon,
+    link: "https://photographer-work.netlify.app/",
+    github: "https://github.com/leobarrientos02/Photon",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "Photon allows users to search the Pexel API for any images they want.",
+    tech: "Javascript",
+  },
+  {
+    name: "Elena Gallery",
+    image: Elena,
+    link: "https://focused-archimedes-c341c5.netlify.app/",
+    github: "https://github.com/leobarrientos02/Photography_Site",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "Beautiful UX/UI design for professional photographers wanting to display their work online.",
+    tech: "UX/UI",
+  },
+  {
+    name: "ColorApp",
+    image: ColorApp,
+    link: "https://color-chooser-app.netlify.app/",
+    github: "https://github.com/leobarrientos02/ColorApp",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "An app that generates color palletes for the user and also allows to visuallize the color patterns wanted for a project.",
+    tech: "Javascript",
+  },
+  {
+    name: "ToDo App",
+    image: Todo,
+    link: "https://affectionate-hamilton-d96896.netlify.app/",
+    github: "https://github.com/leobarrientos02/ToDoApp",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "Simple ToDo App that i developed to master Javascript and dom manipulation.",
+    tech: "Javascript",
+  },
+  {
+    name: "Culture",
+    image: Culture,
+    link: "https://culture-love.netlify.app/",
+    github: "https://github.com/leobarrientos02/WebsiteDesign",
+    technology: ["HTML", "CSS", "JavaScript", "Babel"],
+    description:
+      "Beautiful UX/UI design that uses amazing page transitions to have a good user experience.",
+    tech: "UX/UI",
+  },
+  {
+    name: "Beats App",
+    image: Beats,
+    link: "https://makeyourownbeat.netlify.app/",
+    github: "https://github.com/leobarrientos02/Beat_Maker",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "An adavanced app that allows users to make their own beats using available sounds.",
+    tech: "Javascript",
+  },
+  {
+    name: "Calculator",
+    image: Calc,
+    link: "https://epic-mahavira-ca42eb.netlify.app/",
+    github: "https://github.com/leobarrientos02/Simple_Calculator",
+    technology: ["HTML", "CSS", "JavaScript"],
+    description:
+      "A simple calculator that allows users to do math calculations. Includes Math Laws.",
+    tech: "Javascript",
+  },
 ];
